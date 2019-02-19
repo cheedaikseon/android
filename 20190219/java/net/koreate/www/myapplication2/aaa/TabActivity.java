db5ab008df3ad5529480d1024b15c0bb0f6ea7de
castellanY <kayian0812@gmail.com>
2019-02-19 16:12:17 +0900

    tabs layout

---
+++ 20190219/java/net/koreate/www/myapplication2/aaa/TabActivity.java
@@ -0,0 +1,43 @@
+package net.koreate.www.myapplication2.aaa;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TabHost;
+import android.widget.Toast;
+
+import net.koreate.www.myapplication2.R;
+
+public class TabActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tab);
+        TabHost host = findViewById(R.id.host);
+        host.setup();
+
+        TabHost.TabSpec spec = host.newTabSpec("tab1");
+        spec.setIndicator(null,getResources().getDrawable(R.drawable.tab_icon1));
+        spec.setContent(R.id.tabText1);
+        host.addTab(spec);
+
+        spec = host.newTabSpec("tab2");
+        spec.setIndicator(null,getResources().getDrawable(R.drawable.tab_icon2));
+        spec.setContent(R.id.tabText2);
+        host.addTab(spec);
+
+        spec = host.newTabSpec("tab3");
+        spec.setIndicator(null,getResources().getDrawable(R.drawable.tab_icon3));
+        spec.setContent(R.id.tabText3);
+        host.addTab(spec);
+
+        host.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+            @Override
+            public void onTabChanged(String tabId) {
+                Toast.makeText(TabActivity.this,"변경 : " + tabId,Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+}
