d13d507f62eceb23f6efcd7f8c7fe3937fdb7ff7
castellanY <kayian0812@gmail.com>
2019-02-19 16:11:37 +0900

    Linear Layout prc

---
+++ 20190219/java/net/koreate/www/myapplication2/aaa/LieanrActivity.java
@@ -0,0 +1,15 @@
+package net.koreate.www.myapplication2.aaa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import net.koreate.www.myapplication2.R;
+
+public class LieanrActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lieanr);
+    }
+}
