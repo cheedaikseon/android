c8e42df4572b5bbe3d49e64beab149afc0c5206d
castellanY <kayian0812@gmail.com>
2019-02-19 16:10:03 +0900

    relative activity

---
+++ 20190219/java/net/koreate/www/myapplication2/aaa/RelativeActivity.java
@@ -0,0 +1,16 @@
+package net.koreate.www.myapplication2.aaa;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import net.koreate.www.myapplication2.R;
+
+
+public class RelativeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative);
+    }
+}
